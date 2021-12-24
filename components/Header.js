dc35762770addf355d30e6d16ea5ed10a104acc3
bdoh6 <60983584+bdoh6@users.noreply.github.com>
2021-12-24 17:22:53 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,5 @@
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
-  <div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#about">About</a>
-  <a href="#contact">Contact</a>
   <input type="text" placeholder="Search..">
 </div>
 }
